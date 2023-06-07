4167b3875142554c83a78b74dfd2c77cee65e89c
g4ze <bhaiyajionline17@gmail.com>
2023-06-07 22:55:12 +0530

    ayo new stuff comin in

---
+++ model/model.go
@@ -0,0 +1,11 @@
+package model
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Movies struct {
+	Name    string             `json:"name,omitempty" bson:"name,omitempty"`
+	ID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Watched bool               `json: "watched" bson:"watched"`
+}
